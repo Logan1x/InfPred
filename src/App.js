2ad11c7a1c5afd1030bdff7d765b6b75ea313901
logan1x <sharmakhushal78@gmail.com>
2021-09-16 13:50:13 +0530

    fixed : Breakpoints, md to lg for better viewing

--- src/App.js
+++ src/App.js
@@ -5,8 +5,8 @@ function App() {
   return (
     <div className="bg-purple-50">
       <Nav />
-      <div className="md:w-3/5 mx-auto h-page flex flex-col justify-center items-center border-4 border-red-600">
-        <h1 className="font-bold text-5xl text-indigo-600">
+      <div className="lg:w-3/5 mx-auto h-page flex flex-col justify-center items-center px-3">
+        <h1 className="font-bold text-2xl lg:text-5xl text-indigo-600">
           Welcome to
           <span className="bg-gray-200 px-2 py-1 hover:bg-transparent cursor-wait">
             InfPred
@@ -21,22 +21,22 @@ function App() {
         </p>
       </div>
       {/* colored div */}
-      <div className="w-screen md:h-page bg-gray-300 p-10">
-        <div className="md:w-3/5 mx-auto h-full flex flex-col justify-center items-center py-10 px-6 bg-purple-50 rounded-md">
-          <h1 className="font-bold text-5xl text-indigo-600 my-10">
+      <div className="w-screen lg:h-page bg-gray-300 p-10">
+        <div className="lg:w-3/5 mx-auto h-full flex flex-col justify-center items-center py-10 px-10 bg-purple-50 rounded-lg">
+          <h1 className="font-bold text-2xl lg:text-5xl text-indigo-600 my-10">
             About Influenza
             <span className="bg-gray-200 px-2 py-1 hover:bg-transparent cursor-wait">
               Virus
             </span>
           </h1>
-          <div className="flex flex-wrap md:flex-nowrap md:space-x-6">
-            <p className="text-xl  my-4 bg-white px-14 py-8 shadow rounded text-left">
+          <div className="flex flex-wrap lg:flex-nowrap lg:space-x-6">
+            <p className="text-xl  my-4 bg-white px-14 py-8 shadow rounded text-center lg:text-left">
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic,
               obcaecati odit. Eveniet, unde possimus nobis non debitis illum
               beatae voluptate atque perspiciatis enim facere ipsum dolorem amet
               adipisci, maxime doloremque?
             </p>
-            <p className="text-xl  my-4 bg-white px-14 py-8 shadow rounded text-right">
+            <p className="text-xl  my-4 bg-white px-14 py-8 shadow rounded text-center lg:text-right">
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic,
               obcaecati odit. Eveniet, unde possimus nobis non debitis illum
               beatae voluptate atque perspiciatis enim facere ipsum dolorem amet
